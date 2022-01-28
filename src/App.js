be6fb04711cb8217c45b61171b1a1a4277877dd3
Samsomyajit <somyajitchppr@gmail.com>
2022-01-28 20:04:31 +0530

    new changes

--- src/App.js
+++ src/App.js
@@ -1,23 +1,17 @@
-import logo from './logo.svg';
+//import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
+import Signup from './components/signup/Signup';
+import Footer from './components/Footer';
 import './App.css';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <div className="App">
+        <Signup />
+      </div>
+      <div className="footer-pos">
+       <Footer />
+      </div>
     </div>
   );
 }
---
+++ src/components/signup/Signup.js
@@ -0,0 +1,33 @@
+import React from 'react';
+import './signup.css';
+
+const Signup = () => {
+
+  return (
+    <div className="main-container">
+        <div className='signup-container'>
+            <div className="form-header">
+                <h1>Register</h1>
+            </div>
+            <div className="form-span"></div>
+            <div className="form-input-container">
+            <form>
+                <select className='select' id="admin/user" defaultValue="">
+                    <option className='placeHolder' hidden value="">Enter admin/user</option>
+                    <option value="Admin">Admin</option>
+                    <option value="User">User</option>
+                </select><br />
+                <input type="email" name="email" id="email" placeholder="Enter email"></input><br />
+                <input type="text" id="username" name="username" placeholder="Enter username"></input><br />
+                <input type="tel" name="mobileNumber" id="mobileNumber" pattern="[0-9]{3}" placeholder="Enter Mobilenumber"></input><br />
+                <input type="password" name="password" id="password" placeholder="Password"></input><br />
+                <input type="password" name="confirmPassword" id="confirmPassword" placeholder="Confirm Password"></input><br />
+            </form>
+            <button className="button"><span>Submit </span></button>
+            </div>  
+        </div>
+    </div> 
+  );
+};
+
+export default Signup;
