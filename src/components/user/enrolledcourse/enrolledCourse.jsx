6bd8e83c9bd69f346b9ec26c90fca09a1ca09417
SUREKA-SM <97679982+SUREKA-SM@users.noreply.github.com>
2022-03-06 17:09:37 +0530

    Add files via upload
    enrolled course page of user is added

---
+++ src/components/user/enrolledcourse/enrolledCourse.jsx
@@ -0,0 +1,21 @@
+import { Button } from '@mui/material';
+import React from 'react'
+import './enrolledcourse.css';
+
+const Enrolledcourse = () => {
+  
+    return (
+      <div className='enrolledcoursecontent'>
+       <h1>Course Name :B.E(ECE) <br></br>
+        Joined Date:dd/mm/yyyy<br></br>
+        Course End Date :dd/mm/yyyy<br></br>
+        </h1>
+    <div className='button-alignment'>
+        <button className='enrollbutton'>Mylearning</button>
+        </div>
+        </div>
+      
+    )
+  }
+  
+  export default Enrolledcourse
