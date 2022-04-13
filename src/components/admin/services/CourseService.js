19cd695d15f6b072efd9def5256881c0da5fdeeb
NARAHARI SAISRINIVAS <70793674+vtu13388@users.noreply.github.com>
2022-04-13 18:42:40 +0530

    Delete src directory

--- src/components/admin/services/CourseService.js
+++
@@ -1,9 +0,0 @@
-import axios from 'axios';
-const Add_Course_Url ="";
-
-class CourseService{
-    addCourse(state){
-        return axios.post(Add_Course_Url,state);
-    }
-}
-export default new CourseService();
